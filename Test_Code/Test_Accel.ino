ce8c8470f23ef99fd504352eb190eb516083745f
Feals <39621943+Feals@users.noreply.github.com>
2018-06-05 11:32:08 +0200

    Projet
    Code de test

---
+++ Test_Code/Test_Accel.ino
@@ -0,0 +1,15 @@
+#include <math.h>
+const int x_out = A1; /* axe_x ADXL */
+const int y_out = A2; /* axe_y ADXL */
+const int z_out = A3; /* axe_z ADXL */
+
+void setup (){
+  Serial.begin(9600)
+  //Serial.begon(115200) // À Tester
+  }
+
+void loop(){
+  int x_val_volt, y_val_volt, z_val_volt;
+ 
+
+  }
---
+++ Test_Code/Test_Bluetooth.ino
@@ -0,0 +1,17 @@
+    int a = 10;
+    int b = 20;
+    int c = 45;
+
+    void setup() {
+      // put your setup code here, to run once:
+      Serial1.begin(9600);
+    }
+
+    void loop() {
+      Serial1.print(a);
+      Serial1.print(",");
+      Serial1.print(b);
+      Serial1.print(",");
+      Serial1.print(c);
+      delay(1000);
+  } 
