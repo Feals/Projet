51b4416371506e566d161bf8028f663683bb88a3
Feals <39621943+Feals@users.noreply.github.com>
2018-06-20 15:24:34 +0200

    V2.0

--- Test_Code/Accéléromètre/Test_Accel2/Test_Accel2.ino
+++ Test_Code/Accéléromètre/Test_Accel2/Test_Accel2.ino
@@ -1,18 +1,20 @@
 #include <math.h>
+#include <ADXL335.h>
 const int x_out = A1; /* axe_x Analog */
 const int y_out = A2; /* axe_y Analog */
 const int z_out = A3; /* axe_z Analog */
 
-
+#define pi 3.141592
 void setup () {
   Serial.begin(9600);
 }
 
 void loop() {
 
+
 int xv,yv,zv; //Variable en volt
 double xg,yg,zg; // Variable en g
-double A,G; // A - Accélération _ G - Résultante
+double A,G,Axy,Angle; // A - Accélération _ G - Résultante
 
 //Référence a 3.3 volt (Pin 18=Ref voir datasheet)
 analogReference(18);
@@ -24,21 +26,28 @@ analogReference(18);
 
     //*****Conversion de volt à g *****
         xg=  ( ((double)(xv - 505)*5)/1024);
-        yg=  ( ((double)(yv - 506)*5)/1024);
-        zg=  ( ((double)(zv - 630)*5)/1024);
+        yg=  ( ((double)(yv - 505)*5)/1024);
+        zg=  ( ((double)(zv - 425.3333)*5)/1024);
 
         //Calcul de la résultante
-            G = sqrt(xg*xg + yg*yg + zg*zg);
-
+            G = xg*xg + yg*yg + zg*zg;
+            G=sqrt(G);
           //Calcul de l'accéleration en m/s²
-              A = G * 9,81;
+            A = (G * 9.81);
 
+   Axy=xg*xg+yg*yg;
+   Axy=sqrt(Axy);
+
+Angle = 180.0/pi * atan2(Axy,zg);
 
   Serial.print("Accélération G :");
   Serial.print(G);
-  Serial.print("\t\t\t\t");
+  Serial.print("\t\t");
   Serial.print("Accélération m/s² :");
   Serial.print(A);
+  Serial.print("\t\t");
+  Serial.print("Angle °:");
+  Serial.print(Angle);
   Serial.print("\n");
   delay(100);
 
